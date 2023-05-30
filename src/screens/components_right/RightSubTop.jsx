6e45184a9e0b57b8e44f0c5ec2c5afa170d339e2
kym <dudals896@gmailcom>
2023-05-30 18:52:59 +0900

    commit 5/30 v1

--- src/screens/components_right/RightSubTop.jsx
+++ src/screens/components_right/RightSubTop.jsx
@@ -1,18 +1,70 @@
 import React, {useState} from 'react';
 import "./RightSubTop.css"
-
+import EventGoodComponent from "../../components-new/main/event-good/EventGoodComponent";
+const forss = [
+    {
+        like: "./Body/contents/rightSubTop/newhot_title.gif",
+        manufacturers:"스즈키",
+        img:"./Body/top/gsx-r125.jpeg",
+        name:"gsx-r 125",
+        title:"스즈키의 모델 gsxr125 19년식 관리 잘되어 있습니다. 연락많이 주세요"
+    }
+];
 function RightSubTop(props) {
-    const forss = [
+
+    const goodsLike = [
         {
-            like: "./Body/contents/rightSubTop/newhot_title.gif",
-            manufacturers:"스즈키",
-            img:"./Body/top/gsx-r125.jpeg",
-            name:"gsx-r 125",
-            title:"스즈키의 모델 gsxr125 19년식 관리 잘되어 있습니다. 연락많이 주세요"
+            img: "./Body/contents/rightSubTop/newhot_tab01_none.gif",
+            overImg : "./Body/contents/rightSubTop/newhot_tab01_over.gif",
+            contentImg : "./Body/contents/rightSubTop/newhot_title.gif",
+            name : "보트",
+            title : "고급 보트 팝니다 상태 매우 좋음"
+        },
+        {
+            img: "./Body/contents/rightSubTop/newhot_tab02_none.gif",
+            overImg : "./Body/contents/rightSubTop/newhot_tab02_over.gif",
+            contentImg : "./Body/contents/rightSubTop/cycle.jpeg",
+            name : "보트",
+            title : "고급 보트 팝니다 상태 매우 좋음"
+        },
+        {
+            img: "./Body/contents/rightSubTop/newhot_tab03_none.gif",
+            overImg : "./Body/contents/rightSubTop/newhot_tab02_over.gif",
+            contentImg : "./Body/contents/rightSubTop/boat.jpeg",
+            name : "보트",
+            title : "고급 보트 팝니다 상태 매우 좋음"
+        }
+    ];
+    const subTopDate = [
+        {
+            img: "./Body/contents/rightSubTop/newhot_tab01_none.gif",
+            overImg : "./Body/contents/rightSubTop/newhot_tab01_over.gif",
+            contentImg : "./Body/contents/rightSubTop/newhot_title.gif",
+            name : "보트",
+            title : "고급 보트 팝니다 상태 매우 좋음"
+        },
+        {
+            img: "./Body/contents/rightSubTop/newhot_tab02_none.gif",
+            overImg : "./Body/contents/rightSubTop/newhot_tab02_over.gif",
+            contentImg : "./Body/contents/rightSubTop/cycle.jpeg",
+            name : "보트",
+            title : "고급 보트 팝니다 상태 매우 좋음"
+        },
+        {
+            img: "./Body/contents/rightSubTop/newhot_tab03_none.gif",
+            overImg : "./Body/contents/rightSubTop/newhot_tab02_over.gif",
+            contentImg : "./Body/contents/rightSubTop/boat.jpeg",
+            name : "보트",
+            title : "고급 보트 팝니다 상태 매우 좋음"
         }
     ];
 
-    const [fors, setFors] = useState(forss)
+    const [goodObject, setGoodObject] = useState(subTopDate[0]);
+
+    const onClickEvent = (index) =>{
+        setGoodObject(subTopDate[index])
+    }
+
     return (
         <div className={"rightSubTop"}>
             <ul>
@@ -24,9 +76,14 @@ function RightSubTop(props) {
                         <dd className={"rightThumb"}>
                             <img className={"thumbMain"} src={"./Body/top/gsx-r125.jpeg"}/>
                             <div className={"remoteSubTop"}>
-                                <img src={"./Body/contents/rightSubTop/newhot_tab01_none.gif"}/>
-                                <img src={"./Body/contents/rightSubTop/newhot_tab02_none.gif"}/>
-                                <img src={"./Body/contents/rightSubTop/newhot_tab03_none.gif"}/>
+                                {goodsLike.map((e, index) => {
+                                    const isSelected = index === true;
+
+                                    return <EventGoodComponent
+                                            img={e.img}
+                                            overImg={e.overImg}
+                                    />
+                                })}
                             </div>
                         </dd>
                         <dd className={"rightName"}>
